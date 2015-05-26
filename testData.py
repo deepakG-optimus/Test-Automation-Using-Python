a908edcfa6adf65ddaba6d6c6dac70038753e3d0
deepakTest <deepakkumar.gupta@optimusinfo.com>
2015-05-26 17:20:21 +0530

    adding testData.py

---
+++ testData.py
@@ -0,0 +1,12 @@
+# Flipkart
+flipkart_Url = "http://www.flipkart.com/"
+search_field = "fk-top-search-box"
+search_data = "Moto E"
+#Signup_section
+sign_button = ".//*[@id='fk-mainhead-id']/div[1]/div/div[2]/div[1]/ul/li[8]/a"
+email_field = ".//*[@id='login_email_id']"
+email = "abc@mail.com"
+#Window handle
+cbse_Url = "http://cbse.nic.in/welcome.htm"
+frame_Name = "left"
+hindi_Link = "html/body/div[1]/center/table/tbody/tr[3]/td"
